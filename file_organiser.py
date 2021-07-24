b4ab8bcf8d328a83cf07fa9f6efce5f6fb855f2b
sujoy743175 <sujoy743175@yahoo.co.in>
2021-07-24 22:34:45 +0530

    one '/' removed from '//', still working fine

--- file_organiser.py
+++ file_organiser.py
@@ -1,13 +1,13 @@
 import os, shutil
 
 try:
-    path="//home//robotics//Downloads/book"    
+    path="/home/robotics/Downloads/book"    
     for (path, dirs, files) in os.walk(path):
         for file in files:
             extension=file.split('.')[-1] # will take the last object after dot
             print(extension)
-            source = (path+"//"+file)
-            destination = (path+"//"+extension)
+            source = (path+"/"+file)
+            destination = (path+"/"+extension)
             if os.path.exists(destination):
                 if file.endswith(extension):
                     shutil.move(source, destination)
